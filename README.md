162aca2910d82f68c54dc97f1e5aa29d445e8306
patrykforys <patrykforys0@gmail.com>
2024-09-21 10:24:57 +0200

    zadanie

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# zadanie-kolko
- zadanie
+## Zastępstwa - Powiadomienia Discord
+
+ Ten skrypt w Node.js używa Puppeteer do monitorowania strony internetowej z zastępstwami i powiadamiania na Discordzie o nowych zastępstwach. Skrypt wykonuje automatyczne sprawdzenie co godzinę i wysyła powiadomienia, jeśli wykryje nowe zastępstwa.
+
+## Instalacja
+ npm install puppeteer axios prompt-sync lub npm i
+
+## Używanie
+ Stwórz serwer discord, na którym chcesz otrzymywać powiadomienia oraz Utwórz swojego webhooka.
+
+ Skrypt uruchamia się za pomocą komendy node index.js
---
+++ index.js
@@ -0,0 +1,97 @@
+const puppeteer = require("puppeteer"); // puppeteer do symulowania przeglądarki
+const axios = require("axios"); // axios do obsługi webhooków
+const prompt = require("prompt-sync")(); // prompt-sync do pobierania danych od użytkownika
+
+const pageUrl = "https://zs2ostrzeszow.edupage.org/substitution/";
+const checkInterval = 3600000;
+const webhook = prompt("Podaj webhook: ");
+const schoolClass = prompt("Podaj klasę (Nazwa musi być dokładna): ");
+const webhookUrl = `${webhook}`;
+
+async function checkForUpdates() {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 800 });
+    await page.goto(pageUrl, { waitUntil: "load" });
+    await page.waitForSelector("div.cell", { timeout: 5000 });
+
+    const dateString = getFormattedDate(1);
+    const newSubstitution = await page.evaluate(
+      (dateString) =>
+        Array.from(document.querySelectorAll("div.cell")).some((el) =>
+          el.textContent.includes(dateString)
+        ),
+      dateString
+    );
+
+    if (newSubstitution) {
+      await simulateClickAndGetData(page, dateString);
+    } else {
+      await sendNotification("Brak nowych zastępstw.");
+    }
+    await browser.close();
+  } catch (error) {
+    console.error("blad", error);
+  }
+}
+
+async function simulateClickAndGetData(page, dateString) {
+  try {
+    const result = await page.evaluate((dateString) => {
+      const targetElement = Array.from(
+        document.querySelectorAll("div.cell")
+      ).find((el) => el.textContent.includes(dateString));
+      if (targetElement) targetElement.click();
+      return !!targetElement;
+    }, dateString);
+
+    if (result) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
+      const substitutionsForClass = await page.evaluate(
+        (schoolClass) =>
+          Array.from(document.querySelectorAll(".section .header"))
+            .filter((header) => header.textContent.includes(`${schoolClass}`))
+            .map((header) =>
+              Array.from(header.nextElementSibling.querySelectorAll(".row"))
+                .map((row) => row.textContent.trim())
+                .join("\n")
+            )
+            .join("\n\n"),
+        schoolClass
+      );
+
+      const message = substitutionsForClass.trim()
+        ? `Zastępstwa dla klasy ${schoolClass} na ${dateString}:\n${substitutionsForClass}`
+        : `Brak zastępstw dla klasy ${schoolClass} na ${dateString}`;
+      await sendNotification(message);
+    } else {
+      console.error(`nie znaleziono elementu z tekstem ${dateString}.`);
+    }
+  } catch (error) {
+    console.error("blad", error);
+  }
+}
+
+async function sendNotification(message) {
+  try {
+    await axios.post(webhookUrl, {
+      content: message,
+    });
+    console.log("wiadomosc wyslana");
+  } catch (error) {
+    console.error("blad", error);
+  }
+}
+
+function getFormattedDate(dayOffset = 0) {
+  const today = new Date();
+  today.setDate(today.getDate() + dayOffset);
+  const day = today.getDate();
+  const month = String(today.getMonth() + 1).padStart(2, "0");
+  return `${day}.${month}.`;
+}
+
+checkForUpdates();
+setInterval(checkForUpdates, checkInterval);
